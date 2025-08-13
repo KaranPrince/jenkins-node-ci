f5c4f93c50daa06d1c538078e997ff2abe8ac743
Karan S <148225109+KaranPrince@users.noreply.github.com>
2025-08-13 18:28:32 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 80;
 
 // Serve static HTML
 app.use(express.static(path.join(__dirname, 'app')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'app', 'index.html'));
-});
-
 app.listen(PORT, () => {
-    console.log(`🚀 Node.js server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
