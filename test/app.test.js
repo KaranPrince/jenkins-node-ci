d8196f3e34f2a7c0356ebe5da4c81bb7f572035f
Karan S <148225109+KaranPrince@users.noreply.github.com>
2025-08-21 02:56:42 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app/server"); // make sure server.js exports the app
+const app = require("../app/app"); // now require app.js, not server.js
 
 describe("App basic test", () => {
   it("should return 200 OK from homepage", async () => {
