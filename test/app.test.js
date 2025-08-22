66892376d22ea75548dc9e96d1345f1d32e7327d
Karan S <148225109+KaranPrince@users.noreply.github.com>
2025-08-22 12:41:25 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,8 @@
-const request = require("supertest");
-const app = require("../app/app"); // now require app.js, not server.js
+const request = require('supertest');
+const app = require('../app/server');
 
-describe("App basic test", () => {
-  it("should return 200 OK from homepage", async () => {
-    await request(app).get("/").expect(200);
+describe('App basic test', () => {
+  it('should return 200 OK from homepage', async () => {
+    await request(app).get('/').expect(200);
   });
 });
